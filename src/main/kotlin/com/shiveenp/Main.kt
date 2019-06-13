97a0998e1fae906d71c1ac9ad99cc62fad978456
shiveenpandita <shiveenpandita@gmail.com>
2019-06-13 20:48:11 +1000

    Added file upload and download functionality

--- src/main/kotlin/com/shiveenp/Main.kt
+++ src/main/kotlin/com/shiveenp/Main.kt
@@ -1,20 +1,21 @@
 package com.shiveenp
 
 import io.kweb.Kweb
-import io.kweb.dom.element.creation.ElementCreator
 import io.kweb.dom.element.creation.tags.*
 import io.kweb.dom.element.events.on
 import io.kweb.dom.element.new
 import io.kweb.plugins.fomanticUI.fomantic
 import io.kweb.plugins.fomanticUI.fomanticUIPlugin
+import io.kweb.plugins.fomanticUI.setClasses
 import io.kweb.routing.route
 import io.kweb.state.KVar
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.future.await
 import kotlinx.coroutines.launch
+import java.nio.file.Paths
 
 fun main() {
-    Kweb(port = 12000, plugins = listOf(fomanticUIPlugin)) {
+    Kweb(port = 12001, plugins = listOf(fomanticUIPlugin)) {
         doc.body.new {
 
             route {
@@ -27,23 +28,35 @@ fun main() {
                     val loader = div(mapOf("class" to "ui active centered inline loader"))
                     loader.setAttribute("class", "ui disabled loader")
 
-                    div(fomantic.ui.input).new {
-                        val endpointInput = input(type = InputType.text, placeholder = "Enter S3 Endpoint Url")
-                        val bucketInput = input(type = InputType.text, placeholder = "Enter S3 Bucket Name")
-                        button(mapOf("class" to "ui primary button")).text("Search").on.click {
-                            GlobalScope.launch {
-                                val s3Client =
-                                    S3Client(endpointInput.getValue().await(), bucketInput.getValue().await())
-                                loader.setAttribute("class", "ui active centered inline loader")
-                                keyData.value = s3Client.listAllKeys()
-                                if (keyData.value.isNotEmpty()) {
-                                    loader.setAttribute("class", "ui disabled loader")
+                    div(fomantic.ui.vertical.segment).new {
+                        div(fomantic.ui.input).new {
+                            val endpointInput = input(type = InputType.text, placeholder = "Enter S3 Endpoint Url")
+                            val bucketInput = input(type = InputType.text, placeholder = "Enter S3 Bucket Name")
+                            button(mapOf("class" to "ui primary button")).text("Search").on.click {
+                                GlobalScope.launch {
+                                    val s3Client =
+                                        S3Client(endpointInput.getValue().await(), bucketInput.getValue().await())
+                                    loader.setAttribute("class", "ui active centered inline loader")
+                                    keyData.value = s3Client.listAllKeys()
+                                    if (keyData.value.isNotEmpty()) {
+                                        loader.setAttribute("class", "ui disabled loader")
 
+                                    }
+                                }
+                            }
+
+                            val inputFile= input(type = InputType.file, placeholder = "Upload File...")
+                            button(mapOf("class" to "ui primary button")).text("Upload File").on.click {
+                                GlobalScope.launch {
+                                    val s3Client =
+                                        S3Client(endpointInput.getValue().await(), bucketInput.getValue().await())
+                                        loader.setAttribute("class", "ui active centered inline loader")
+                                    s3Client.put(Paths.get(inputFile.getValue().await().replace("C:\\fakepath\\", "")).toFile())
+                                    loader.setAttribute("class", "ui disabled loader")
                                 }
                             }
                         }
                     }
-
 
                     table(mapOf("class" to "ui celled striped table")).new {
                         thead().new {
@@ -57,7 +70,9 @@ fun main() {
                             keyData.map {
                                 it.forEach {
                                     tr().new {
-                                        td(mapOf("data-lable" to "Key")).text(it.key)
+                                        td(mapOf("data-lable" to "Key")).innerHTML("<i class=\"file outline icon\"></i> <a href=${it.downloadUrl}>${it.key}</a>").on.click {
+                                            println("something clicked")
+                                        }
                                         td(mapOf("data-lable" to "File Size (in KB)")).text(it.size.toString())
                                         td(mapOf("data-lable" to "Last Modified At")).text(it.lastModifedAt)
 
--- src/main/kotlin/com/shiveenp/S3Client.kt
+++ src/main/kotlin/com/shiveenp/S3Client.kt
@@ -2,8 +2,11 @@ package com.shiveenp
 
 import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.CannedAccessControlList
 import com.amazonaws.services.s3.model.ListObjectsV2Request
+import com.amazonaws.services.s3.model.SetBucketAclRequest
 import java.io.File
+import java.util.*
 
 class S3Client(private val endpoint: String, private val bucketName: String) {
 
@@ -13,25 +16,33 @@ class S3Client(private val endpoint: String, private val bucketName: String) {
         .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(endpoint, "ap-southeast-2"))
         .build()
 
+    init {
+        client.setBucketAcl(
+            SetBucketAclRequest(
+                bucketName, CannedAccessControlList.PublicRead
+            )
+        )
+    }
+
     fun listAllKeys(): List<S3Data> {
         val req = ListObjectsV2Request().withBucketName(bucketName).withMaxKeys(10)
         val keyList = mutableListOf<S3Data>()
         client.listObjectsV2(req).objectSummaries.forEach {
-           keyList.add(S3Data(it.key, it.size/1000, it.lastModified.toString()))
+            keyList.add(S3Data(it.key, "$endpoint/$bucketName/${it.key}", it.size / 1000, it.lastModified.toString()))
         }
         return keyList
     }
 
     // Just a test function
-    fun put() {
-        val testFile = File("test-file")
-        testFile.createNewFile()
-        client.putObject(bucketName, "test-1", testFile)
+    fun put(file: File) {
+        println("uploading file: ${file.name}")
+        client.putObject(bucketName, "${UUID.randomUUID()}", file)
     }
 }
 
 data class S3Data(
     val key: String,
+    val downloadUrl: String,
     val size: Long,
     val lastModifedAt: String
 )
