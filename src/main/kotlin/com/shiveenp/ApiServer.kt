9635e78f584a5383c0f260ea7536fed44cf19068
shiveenpandita <shiveenpandita@gmail.com>
2019-07-23 20:17:39 +1000

    refactor input and table segments into separate functions

--- src/main/kotlin/com/shiveenp/ApiServer.kt
+++ src/main/kotlin/com/shiveenp/ApiServer.kt
@@ -1,6 +1,8 @@
 package com.shiveenp
 
 import io.kweb.Kweb
+import io.kweb.dom.element.Element
+import io.kweb.dom.element.creation.ElementCreator
 import io.kweb.dom.element.creation.tags.*
 import io.kweb.dom.element.events.on
 import io.kweb.dom.element.new
@@ -34,52 +36,69 @@ private fun startKweb(herokuPort: String?) {
 
                         val loader = div(mapOf("class" to "ui active text loader")).addText("Retrieving keys...")
                         loader.setAttribute("class", "ui disabled text loader")
-
-                        div(fomantic.ui.vertical.segment).new {
-                            div(fomantic.ui.input).new {
-                                val endpointInput = input(type = InputType.text, placeholder = "Enter S3 Endpoint Url")
-                                val bucketInput = input(type = InputType.text, placeholder = "Enter S3 Bucket Name")
-                                button(mapOf("class" to "ui primary button")).text("Search").on.click {
-                                    GlobalScope.launch {
-                                        loader.setAttribute("class", "ui active text loader")
-                                        val s3Client =
-                                            S3Client(endpointInput.getValue().await(), bucketInput.getValue().await())
-                                        try {
-                                            keyData.value = s3Client.listAllKeys()
-                                        } catch (ex: Exception) {
-                                            loader.setAttribute("class", "ui disabled text loader")
-                                        }
-                                        if (keyData.value.isNotEmpty()) {
-                                            loader.setAttribute("class", "ui disabled text loader")
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
-                        table(mapOf("class" to "ui celled striped table")).new {
-                            thead().new {
-                                tr().new {
-                                    th().text("Key")
-                                    th().text("File Size (in KB)")
-                                    th().text("Last Modified At")
-                                }
-                            }
-                            tbody().new {
-                                keyData.map {
-                                    it.forEach {
-                                        tr().new {
-                                            td(mapOf("data-lable" to "Key")).innerHTML("<i class=\"file outline icon\"></i> <a target=\"_blank\" href=${it.downloadUrl} download=${it.key}>${it.key}</a>")
-                                            td(mapOf("data-lable" to "File Size")).text("${it.size} KB")
-                                            td(mapOf("data-lable" to "Last Modified At")).text(it.lastModifiedAt)
-
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                        createInputSegment(loader, keyData)
+                        createKeysTable(keyData)
                     }
 
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Creates the input area where the users can enter the details about the S3 bucket
+ */
+private fun ElementCreator<DivElement>.createInputSegment(
+    loader: Element,
+    keyData: KVar<List<S3Data>>
+) {
+    div(fomantic.ui.vertical.segment).new {
+        div(fomantic.ui.input).new {
+            val endpointInput = input(type = InputType.text, placeholder = "Enter S3 Endpoint Url")
+            val bucketInput = input(type = InputType.text, placeholder = "Enter S3 Bucket Name")
+            button(mapOf("class" to "ui primary button")).text("Search").on.click {
+                GlobalScope.launch {
+                    loader.setAttribute("class", "ui active text loader")
+                    val s3Client =
+                        S3Client(endpointInput.getValue().await(), bucketInput.getValue().await())
+                    try {
+                        keyData.value = s3Client.listAllKeys()
+                    } catch (ex: Exception) {
+                        loader.setAttribute("class", "ui disabled text loader")
+                    }
+                    if (keyData.value.isNotEmpty()) {
+                        loader.setAttribute("class", "ui disabled text loader")
+                    }
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Renders the table of s3 keys using the given key data map
+ */
+private fun ElementCreator<DivElement>.createKeysTable(
+    keyData: KVar<List<S3Data>>
+) {
+    table(mapOf("class" to "ui celled striped table")).new {
+        thead().new {
+            tr().new {
+                th().text("Key")
+                th().text("File Size (in KB)")
+                th().text("Last Modified At")
+            }
+        }
+        tbody().new {
+            keyData.map {
+                it.forEach {
+                    tr().new {
+                        td(mapOf("data-lable" to "Key")).innerHTML("<i class=\"file outline icon\"></i> <a target=\"_blank\" href=${it.downloadUrl} download=${it.key}>${it.key}</a>")
+                        td(mapOf("data-lable" to "File Size")).text("${it.size} KB")
+                        td(mapOf("data-lable" to "Last Modified At")).text(it.lastModifiedAt)
+
+                    }
                 }
             }
         }
